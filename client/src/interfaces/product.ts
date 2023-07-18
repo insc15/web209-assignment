3f497482aa2ea73ebb8a53e0fcca52a145409204
hieunm156 <hieunm156@gmail.com>
2023-07-18 20:14:35 +0700

    update

--- client/src/interfaces/product.ts
+++ client/src/interfaces/product.ts
@@ -1,8 +1,16 @@
 export default interface IProduct{
-    id?: string | number;
+    id: string | number;
+    image: string;
     name: string;
-    author: string;
     price: number;
     discount_price?: number;
-    image: string;
+    short_description?: string;
+    category_id: number;
+    stock: number;
+    author: string;
+    description: string;
+    publisher?: string;
+    page_num?: number;
+    publishing_year?: number;
+    language?: string;
 }
