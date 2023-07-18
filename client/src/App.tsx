67a216a78889b07b12399a42f800e7b5a7871d90
VuXuanNam <nvu324847@gmail.com>
2023-07-19 00:39:16 +0700

    crud cate

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,11 @@ import PageShop from "./pages/shop";
 import PageAdminDashboard from "./pages/admin/dashboard";
 import PageAdminProducts from "./pages/admin/product";
 import PageAdminCreateProducts from "./pages/admin/product/create";
+import Categories from "./pages/admin/Categories";
+import PageAdminCreateCategories from "./pages/admin/category/create";
+import PageAdminCategories from "./pages/admin/category";
+import PageAdminUpdateCategories from "./pages/admin/category/update";
+
 
 function App() {
   return <BrowserRouter>
@@ -12,6 +17,7 @@ function App() {
       <Route path="/" element={<DefaultLayout />} >
         <Route index element={<PageHome />} />
         <Route path="/shop" element={<PageShop />} />
+
       </Route>
       <Route path="admin" element={<AdminLayout />} >
         <Route index element={<PageAdminDashboard />} />
@@ -19,6 +25,12 @@ function App() {
           <Route index element={<PageAdminProducts />} />
           <Route path="create" element={<PageAdminCreateProducts />} />
         </Route>
+        <Route path="categories">
+          <Route index element={<PageAdminCategories />} />
+          <Route path="create" element={<PageAdminCreateCategories />} />
+          <Route path="update/:id" element={<PageAdminUpdateCategories />} />
+        </Route>
+
       </Route>
     </Routes>
   </BrowserRouter>
--- client/src/layout/header.tsx
+++ client/src/layout/header.tsx
@@ -2,30 +2,53 @@ import Container from "../components/layout/container";
 import { BsTelephoneInbound, BsPinterest, BsInstagram, BsFacebook, BsHandbag, BsHeart, BsPerson, BsGeoAlt } from "react-icons/bs";
 import { BiEnvelope, BiSearch, BiPhoneIncoming } from "react-icons/bi";
 import { FaGithub } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import logo from "../assets/logo.svg";
 import CategorySelector from "./category.header";
+import { useState, useEffect, useRef } from "react";
 
 function Header() {
+    const [searchKeyword, setSearchKeyword] = useState('');
+    // const location = useLocation();
+    const navigate = useNavigate();
+    // const [path, setPath] = useState<string>('');
+    // const headerRef = useRef(null);
+    // useEffect(() => {
+    //     setPath(location.pathname);
+    // }, [location.pathname]);
+
+    const handleSearchInputChange = (event: any) => {
+        setSearchKeyword(event.target.value);
+    };
+
+    const handleSearchSubmit = (event: any) => {
+        event.preventDefault();
+        const searchParams = new URLSearchParams();
+        searchParams.append('', searchKeyword);
+        navigate({
+            pathname: 'search',
+            search: searchParams.toString()
+        });
+    };
     return (
         <header className="">
             <div className="bg-[#111214] text-gray-300 text-xs">
                 <Container className="py-3 flex justify-between items-center">
                     <div className="flex items-center">
                         <p className="flex items-center">
-                            <BsTelephoneInbound className="text-base"/>
+                            <BsTelephoneInbound className="text-base" />
                             <span className="ml-2">+(84) - 1800 - 4635</span>
                         </p>
                         <p className="flex items-center ml-4">
-                            <BiEnvelope className="text-base"/>
+                            <BiEnvelope className="text-base" />
                             <span className="ml-2">info@bookstore.com</span>
                         </p>
                     </div>
                     <ul className="flex items-center">
-                        <li className="ml-5"><a href="https://github.com/programeoww" target="_blank"><FaGithub className="text-xl"/></a></li>
-                        <li className="ml-5"><a href="#" target="_blank"><BsPinterest className="text-xl"/></a></li>
-                        <li className="ml-5"><a href="#" target="_blank"><BsInstagram className="text-xl"/></a></li>
-                        <li className="ml-5"><a href="#" target="_blank"><BsFacebook className="text-xl"/></a></li>
+                        <li className="ml-5"><a href="https://github.com/programeoww" target="_blank"><FaGithub className="text-xl" /></a></li>
+                        <li className="ml-5"><a href="#" target="_blank"><BsPinterest className="text-xl" /></a></li>
+                        <li className="ml-5"><a href="#" target="_blank"><BsInstagram className="text-xl" /></a></li>
+                        <li className="ml-5"><a href="#" target="_blank"><BsFacebook className="text-xl" /></a></li>
                     </ul>
                 </Container>
             </div>
@@ -33,28 +56,31 @@ function Header() {
                 <Container className="py-5 flex justify-between items-center">
                     <div className="">
                         <Link to="/">
-                            <img src={logo} alt="logo" className="w-44"/>
+                            <img src={logo} alt="logo" className="w-44" />
                         </Link>
                     </div>
                     <div className="flex items-center w-3/4 justify-end">
-                        <label className="relative bg-gray-100 rounded-full w-full">
-                            <BiSearch size="22" className="text-gray-400 mr-3 absolute top-1/2 -translate-y-1/2 left-5"/>
-                            <input className="bg-transparent outline-none w-full px-14 py-3" type="text" placeholder="Search products..."/>
-                            <button className="absolute top-0 bottom-0 bg-primary hover:bg-button-hover duration-150 rounded-full px-9 right-0 text-white">
-                                <BiSearch size="22"/>
-                            </button>
-                        </label>
-                        <p className="mx-8 hover:underline cursor-pointer shrink-0"><BsGeoAlt className="inline" size="20"/> Find a book store</p>
+                        <form action="" onSubmit={handleSearchSubmit}>
+                            <label className="relative bg-gray-100 rounded-full w-full">
+                                <BiSearch size="22" className="text-gray-400 mr-3 absolute top-1/2 -translate-y-1/2 left-5" />
+                                <input value={searchKeyword}
+                                    onChange={handleSearchInputChange} className="bg-transparent outline-none w-full px-14 py-3" type="text" placeholder="Search products..." />
+                                <button className="absolute top-0 bottom-0 bg-primary hover:bg-button-hover duration-150 rounded-full px-9 right-0 text-white">
+                                    <BiSearch size="22" />
+                                </button>
+                            </label>
+                        </form>
+                        <p className="mx-8 hover:underline cursor-pointer shrink-0"><BsGeoAlt className="inline" size="20" /> Find a book store</p>
                         <ul className="flex items-center">
                             <li className="relative ml-5 cursor-pointer hover:text-primary duration-150">
-                                <BsPerson size="26"/>
+                                <BsPerson size="26" />
                             </li>
                             <li className="relative ml-5 cursor-pointer hover:text-primary duration-150">
-                                <BsHeart size="22"/>
+                                <BsHeart size="22" />
                                 <span className="absolute top-0 right-0 rounded-full bg-gray-600 text-white p-px text-xs w-4 h-4 text-center translate-x-1/3 -translate-y-1/4 outline-white outline">0</span>
                             </li>
                             <li className="relative ml-5 cursor-pointer hover:text-primary duration-150">
-                                <BsHandbag size="22"/>
+                                <BsHandbag size="22" />
                                 <span className="absolute top-0 right-0 rounded-full bg-gray-600 text-white p-px text-xs w-4 h-4 text-center translate-x-1/3 -translate-y-1/4 outline-white outline">0</span>
                             </li>
                         </ul>
@@ -63,7 +89,7 @@ function Header() {
             </div>
             <Container className="py-5 flex items-center">
                 <div className="w-2/12">
-                    <CategorySelector/>
+                    <CategorySelector />
                 </div>
                 <nav className="w-10/12 flex items-center justify-end">
                     <ul className="flex justify-center font-medium w-full">
@@ -75,7 +101,7 @@ function Header() {
                     </ul>
                     <div className="flex items-center shrink-0">
                         <div className="p-2 rounded-full bg-gray-200 text-primary">
-                            <BiPhoneIncoming className="text-2xl"/>
+                            <BiPhoneIncoming className="text-2xl" />
                         </div>
                         <p className="ml-5">
                             <span className="block font-semibold text-primary">+(84) - 1800 - 4635</span>
